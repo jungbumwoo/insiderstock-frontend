68e1500c9792c9a2c1bc6137a585581622c25c30
jungbumwoo <jungbumwoo@naver.com>
2021-05-12 21:29:13 +0900

    Modal Open and Close

--- src/components/Modals/Modal/Modal.js
+++ src/components/Modals/Modal/Modal.js
@@ -5,32 +5,31 @@ import Icon from "../../../static/images/x-mark.svg";
 const Modal = (props) => {
     let shown = props.shown;
     let onCloseRequest = props.onCloseRequest;
-    console.log(shown);
-    useEffect(() => {
-        if (shown) {
-            const backGround = document.querySelector('.profile_modal');
-            const modal_main = document.querySelector('.modal-main');
-            backGround.addEventListener('click', (event) => {
-                console.log("clicked !!!");
-                onCloseRequest();
-            });
-            modal_main.addEventListener('click', (event) => {
-                event.stopPropagation();
-            })
-        }
-    }, []);
+    // useEffect(() => {
+    //     if (shown) {
+    //         const backGround = document.querySelector('.profile_modal');
+    //         const modal_main = document.querySelector('.modal-main');
+    //         backGround.addEventListener('click', (event) => {
+    //             console.log("clicked !!!");
+    //             onCloseRequest();
+    //         });
+    //         modal_main.addEventListener('click', (event) => {
+    //             event.stopPropagation();
+    //         })
+    //     }
+    // }, []);
     
     if (!shown) {
         return null;
     }
     
-    const handleModalBtn = () => {
-        console.log("handleModalBtn");
-    }
-
     return (
-        <div className="profile_modal" id="div_back">
-            <div className="modal-main">
+        <div className="profile_modal" onClick={() => {
+            onCloseRequest();
+        }}>
+            <div className="modal-main" onClick={(e) => {
+                e.stopPropagation();
+            }}>
                 <div className="modal_container">
                     <label>Ticker :</label>
                     <input type="text"/>

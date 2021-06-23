05cf078fe5257f696a10225208ceaa502e718526
jungbumwoo <jungbumwoo@naver.com>
2021-06-23 13:23:18 +0900

    deploy

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = window.localStorage.getItem('token');
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:2000/api",
+    baseURL: "https://limitless-island-44318.herokuapp.com/api",
     headers: {
         'authorization' : token ? `Bearer ${token}` : ''
     }
